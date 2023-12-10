3c2e3c7ed7aa1fea45866eb2a4a92942020d507d
Michael <mihali5@mail.ru>
2023-12-10 10:08:02 +0300

    feat: Добавил ErrorBoundary Для всех страниц

--- src/components/SuspensedComponent/SuspensedComponent.tsx
+++ src/components/SuspensedComponent/SuspensedComponent.tsx
@@ -1,7 +1,12 @@
+import ErrorBoundary from 'components/ErrorBoundary';
 import React, { FC, PropsWithChildren, Suspense } from 'react';
 
 const SuspensedComponent: FC<PropsWithChildren> = ({ children }) => {
-  return <Suspense fallback='Loading...'>{children}</Suspense>;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback='Loading...'>{children}</Suspense>
+    </ErrorBoundary>
+  );
 };
 
 export default SuspensedComponent;
